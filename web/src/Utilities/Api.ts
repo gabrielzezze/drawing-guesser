781b556851e2763227c8e6a51e36e6c1e2b05f2d
gabrielzezze <gabrielzezze@me.com>
2021-06-09 15:46:15 -0300

    Folder re-do

---
+++ web/src/Utilities/Api.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const Api = axios.create({
+    baseURL: process.env.NODE_ENV ? 'http:localhost:5000': '',
+    headers: {
+        'ContentType': 'application/json'
+    }
+})
+
+
+export interface RequestWrapperArgs {
+    url: string
+    method: 'get' | 'post'
+    data: any
+}
+
+export async function requestWrapper<T>(args: RequestWrapperArgs) {
+    const {
+        url,
+        method,
+        data
+    } = args
+
+    try {
+        const res = await Api.request({
+            method,
+            url,
+            data
+        })
+
+        return { success: true, data: res.data as T }
+    }
+    catch (e) {
+        console.log(e)
+
+        return { success: false, data: e.response }
+    }
+}
+
